32a80ad5f6bcbf4cd6572a91d4c703bc2bfd351f
macgrubbers <mckay.murphy95@gmail.com>
2026-03-30 21:36:57 -0600

    dialogue system framework ish
     (prolly needs some work but we shall get to that inshallah)

---
+++ Scenes/Actor Components/dialogue_manager_component.gd
@@ -0,0 +1,22 @@
+extends Node
+class_name DialogueManagerComponent
+
+@export var starting_dialogue_node:DialogueNode
+var current_dialogue_node:DialogueNode = starting_dialogue_node
+var prev_dialogue_node:DialogueNode
+
+# Get the dialogue text of the current node
+func get_current_dialogue_node_text():
+	return current_dialogue_node.dialogue_text
+	
+func get_current_dialogue_node_state():
+	return current_dialogue_node.dialogue_state
+
+# Get the responses of the current node
+func get_current_dialogue_node_responses():
+	return current_dialogue_node.dialogue_responses
+
+# Switch to a new dialogue node
+func switch_dialogue_nodes(new_node:DialogueNode):
+	prev_dialogue_node = current_dialogue_node
+	current_dialogue_node = new_node
---
+++ Scenes/Actor Components/dialogue_node.gd
@@ -0,0 +1,10 @@
+extends Node
+class_name DialogueNode
+
+# Variables
+## The text that will be "said" by the actor.
+@export var dialogue_text:String
+## The new state for the actor to transition to, leave empty if none.
+@export var dialogue_state:String
+## The dictionary holding dialogue responses for the player to choose from. These are organized in pairs of linked Nodes and dialogue "responses".
+@export var dialogue_responses:Dictionary[DialogueNode, String]
