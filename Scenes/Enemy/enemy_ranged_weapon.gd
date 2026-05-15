e27f0118dbd5d92ff8c350f0d90bb9ed3dc0bb40
Mac Wack Daddy <55512770+macgrubbers@users.noreply.github.com>
2026-05-14 20:15:17 -0600

    bullet working
    no reflect yet

--- Scenes/Enemy/enemy_ranged_weapon.gd
+++ Scenes/Enemy/enemy_ranged_weapon.gd
@@ -6,8 +6,8 @@ extends MeshInstance3D
 
 func fire(target_pos:Vector3):
 	var new_projectile = projectile.instantiate()
-	new_projectile.setup(barrel.global_position, target_pos)
-	new_projectile.shooter = self
 	get_tree().current_scene.add_child(new_projectile)
+	new_projectile.setup(barrel.global_position, target_pos + Vector3(0,1,0))
+	new_projectile.shooter = self
 	
 	
--- Scenes/Projectiles/test_bullet.gd
+++ Scenes/Projectiles/test_bullet.gd
@@ -6,13 +6,18 @@ extends Area3D
 @onready var reflect_speed_multiplier:float
 
 func setup(start_position:Vector3, target:Vector3):
-	global_position = start_position
-	direction = -global_position.direction_to(target)
-	print(direction)
+	set_global_position(start_position)
+	direction = global_position.direction_to(target)
+	connect("area_entered", _on_area_entered)
 
 func _physics_process(delta: float) -> void:
 	global_position += speed * direction * delta
+	
 
 func reflect_at_shooter():
 	direction = global_position.direction_to(shooter.get_global_position())
 	speed *= reflect_speed_multiplier
+
+func _on_area_entered(area:Area3D)->void:
+	if area.get_owner() != shooter and area is HealthComponent:
+		area.change_health(-1,shooter)
