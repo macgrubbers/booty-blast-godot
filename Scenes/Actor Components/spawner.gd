590b3c0a73455aa8f36b678572c950e43bfe5dac
macgrubbers <mckay.murphy95@gmail.com>
2026-05-31 20:29:18 -0600

    COIN RAIN
    fix this later

--- Scenes/Actor Components/spawner.gd
+++ Scenes/Actor Components/spawner.gd
@@ -16,7 +16,10 @@ func spawn(obj_type: PackedScene, amount:int = 1):
 			new_obj.set_physicsable(make_physicsable)
 		get_tree().current_scene.add_child(new_obj)
 		if current_spawn_method == spawn_methods.SCATTER and new_obj.has_method("apply_central_impulse"):
-			new_obj.apply_central_impulse(Vector3(randf_range(x_impulse_range.x, x_impulse_range.y),
+			new_obj.apply_impulse(Vector3(randf_range(x_impulse_range.x, x_impulse_range.y),
+													randf_range(z_impulse_range.x, z_impulse_range.y),
+													randf_range(y_impulse_range.x, y_impulse_range.y)),
+								Vector3(randf_range(x_impulse_range.x, x_impulse_range.y),
 													randf_range(z_impulse_range.x, z_impulse_range.y),
 													randf_range(y_impulse_range.x, y_impulse_range.y)))
 
--- Scenes/Props/chest.gd
+++ Scenes/Props/chest.gd
@@ -1,17 +1,39 @@
 extends AnimatableBody3D
 
+@export var contained_coins: int = 30
+@export var contained_gems: int = 10
+
 @onready var spawner: Spawner = $Spawner
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var coin_scene:PackedScene = preload("res://Scenes/Collectables/CoinCollectable.tscn")
+@onready var gem_scene:PackedScene = preload("res://Scenes/Collectables/GemCollectable.tscn")
 
+@onready var start_spawning:bool = false
 
-func interact():
-	spawn()
-	queue_free()
-
-func spawn():
+func _ready() -> void:
 	spawner.set_spawn_method(Spawner.spawn_methods.SCATTER, 
 						true,
 						Vector2(-0.1,0.1),
 						Vector2(1,1.5),
 						Vector2(-0.1,0.1))
-	spawner.spawn(coin_scene, 10)
+
+func interact():
+	animation_player.play("open")
+	await animation_player.animation_finished
+	start_spawning = true
+
+func _physics_process(delta: float) -> void:
+	if start_spawning:
+		var spawned_object = coin_scene
+		# 1-in-4 probability we spawn a diamond instead
+		if randi_range(0,4) > 3 and contained_gems > 0:
+			spawned_object = gem_scene
+		if contained_coins <= 0:
+			spawned_object = gem_scene
+			
+		spawner.set_spawn_method(Spawner.spawn_methods.SCATTER, 
+							true,
+							Vector2(-0.1,0.1),
+							Vector2(1,1.5),
+							Vector2(-0.1,0.1))
+		spawner.spawn(spawned_object, 1)
