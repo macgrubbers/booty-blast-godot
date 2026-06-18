5836bb2ae160993a2a37027d3896b343fa30c52d
macgrubbers <mckay.murphy95@gmail.com>
2026-06-17 20:18:25 -0600

    spike ball

--- Scenes/Structures/Destructable Structures/base_destructable_physics_object.gd
+++ Scenes/Structures/Destructable Structures/base_destructable_physics_object.gd
@@ -1,31 +1,12 @@
-class_name BaseDestructablePhysicsObject extends RigidBody3D
+class_name DestructableRigidBody3D extends RigidBody3D
 
-@onready var health: int = 3
-@export var scale_override: Vector3 = Vector3(1,1,1)
+@export var health: int = 1
+@export var armor: int = 0
 
-@onready var collision_shape: CollisionShape3D = $CollisionShape3D
-@onready var mesh = $MeshInstance3D
-@onready var spawner: Spawner = $Spawner
-
-
-func _ready() -> void:
-	collision_shape.scale = scale_override
-	mesh.scale = scale_override
-
-func _physics_process(delta: float) -> void:
-	spawner.global_position = global_position
 
 func attack(damage:int, knockback:Vector3, collision_point:Vector3):
 	apply_impulse(knockback,to_local(collision_point))
 	kill()
 	
 func kill():
-	for n in range(10):
-		var new_obj = spawner.spawn()
-	#spawner.set_spawn_method(Spawner.spawn_methods.SCATTER, 
-							#true,
-							#Vector2(-1,1),
-							#Vector2(0,1),
-							#Vector2(-1,1))
-	#spawner.spawn(coin_scene, 10)
-	queue_free()
+	pass
