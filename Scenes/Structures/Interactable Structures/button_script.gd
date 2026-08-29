0945e86f6a352c7be6e34ba670530b583d663dd4
macgrubbers <mckay.murphy95@gmail.com>
2026-08-28 20:31:34 -0600

    level 0 work, moving platform

--- Scenes/Structures/Interactable Structures/button_script.gd
+++ Scenes/Structures/Interactable Structures/button_script.gd
@@ -5,8 +5,10 @@ class_name button
 @export var linked_object:Node
 @export var button_delay:float
 
+signal pressed
 
 
 func interact():
+	pressed.emit()
 	if linked_object and linked_object.has_method("activate"):
 		linked_object.activate()
--- Scenes/Structures/Interactable Structures/moving_platform.gd
+++ Scenes/Structures/Interactable Structures/moving_platform.gd
@@ -1,11 +1,53 @@
-extends AnimatableBody3D
+extends PathFollow3D
 
+enum move_mode{LOOP, TOGGLEABLE_LOOP, TOGGLEABLE_INCREMENT}
+@export var current_mode:move_mode
+@export var path:Path3D
+@export var current_point_index:int
+
+@export_group("Toggleable Mode Options")
+@export var toggle_forwards:Node3D
+@export var toggle_backwards:Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	path = get_parent()
+	if (current_mode == move_mode.TOGGLEABLE_LOOP or 
+		current_mode == move_mode.TOGGLEABLE_INCREMENT):
+			setup_toggleable()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Setup movement for toggleable mode
+func setup_toggleable():
+	if toggle_forwards.has_signal("pressed"):
+		toggle_forwards.connect("pressed", _on_forwards_pressed)
+	
+	if toggle_backwards.has_signal("pressed"):
+		toggle_backwards.connect("pressed", _on_backwards_pressed)
+
+func _on_forwards_pressed():
+	print("Forwards pressed")
+	move_toggleable_increment(1)
+
+func _on_backwards_pressed():
+	print("Backwards pressed")
+	move_toggleable_increment(-1)
+
+func move_toggleable_increment(next_index_dir:int):
+	var curve: Curve3D = path.curve
+	var total_points: int = curve.point_count
+	
+	# Increment the index and wrap around if the path loops
+	var next_point_index = current_point_index + next_index_dir
+	if ((next_point_index <= 0) or
+		(next_point_index > total_points - 1)):
+		print ("Can't move!")
+		return
+	else:
+		current_point_index = next_point_index
+
+	# Get the distance along the path for the specific point index
+	var point_offset: float = curve.get_offset(current_point_index)
+	
+	# Update the PathFollow3D position instantly
+	progress = point_offset
